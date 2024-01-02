b03cdc28f8fc16c03bb8d7f89dda418d13a9e5a0
Misbah Sohail <msbh.sohail@gmail.com>
2024-01-02 22:56:29 +0100

    Updated height at the root tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import "./App.css";
+import BackDrop from "./components/body/backdrop/BackDrop";
 import Header from "./components/header/Header";
 import SubHeader from "./components/sub header/SubHeader";
 
 function App() {
-
-  return <div>
-    <Header/>
-    <SubHeader/>
-  </div>;
+  return (
+    <div className="h-full">
+      <Header />
+      <SubHeader />
+      <BackDrop />
+    </div>
+  );
 }
 
 export default App;
