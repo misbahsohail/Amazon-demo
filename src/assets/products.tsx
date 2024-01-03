6ae1570b86339c4e7a1a24e4d88d749af1cea557
Misbah Sohail <msbh.sohail@gmail.com>
2024-01-03 17:56:03 +0100

    Updated products asset to add discount field

--- src/assets/products.tsx
+++ src/assets/products.tsx
@@ -5,12 +5,13 @@ export const products = [
     category: "Gaming",
     price: 250,
     discountAvailable: false,
-    discountPercentage: 0,
+    discountPercentage: 50,
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
     photos: [
       "https://gmedia.playstation.com/is/image/SIEPDC/playstation-5-with-dualsense-front-product-shot-01-ps5-en-30jul20?$native--t$",
     ],
+    views: 1,
   },
   {
     name: "MacBook Pro 13",
@@ -18,12 +19,13 @@ export const products = [
     category: "Computers",
     price: 2299,
     discountAvailable: false,
-    discountPercentage: 0,
+    discountPercentage: 10,
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
     photos: [
       "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16touch-space-select-201911?wid=904&hei=840&fmt=jpeg&qlt=80&op_usm=0.5,0.5&.v=1572825197207",
     ],
+    views: 4,
   },
   {
     name: "MacBook Pro 16",
@@ -31,12 +33,13 @@ export const products = [
     category: "Computers",
     price: 2500,
     discountAvailable: false,
-    discountPercentage: 0,
+    discountPercentage: 10,
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
     photos: [
       "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16touch-space-select-201911?wid=904&hei=840&fmt=jpeg&qlt=80&op_usm=0.5,0.5&.v=1572825197207",
     ],
+    views: 4,
   },
   {
     name: "IPhone X",
@@ -44,11 +47,12 @@ export const products = [
     category: "Technology/mobile phones",
     price: 1200,
     discountAvailable: false,
-    discountPercentage: 0,
+    discountPercentage: 35,
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
     photos: [
       "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-xr-select-2019-family?wid=441&amp;hei=529&amp;fmt=jpeg&amp;qlt=95&amp;op_usm=0.5,0.5&amp;.v=1550795424612",
     ],
+    views: 2,
   },
 ];
